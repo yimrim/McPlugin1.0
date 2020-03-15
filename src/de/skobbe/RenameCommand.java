6cb523ca621cfc30558a32513567a43c396fdf35
SoulFoxer <leon.wichelmann@web.de>
2020-03-15 21:30:31 +0100

    renamed RenameCommand to /rename ingame and implemented the full functionality

--- src/de/skobbe/RenameCommand.java
+++ src/de/skobbe/RenameCommand.java
@@ -1,5 +1,9 @@
 package de.skobbe;
 
+import net.minecraft.server.v1_15_R1.EntityPlayer;
+import net.minecraft.server.v1_15_R1.PlayerChunkMap;
+import net.minecraft.server.v1_15_R1.WorldServer;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -7,12 +11,22 @@ import org.bukkit.entity.Player;
 
 public class RenameCommand implements CommandExecutor {
 
+    /**
+     * param string[0]   playerName
+     * param string[1]   choosenName
+     */
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            player.sendMessage(sender.getName()+" wurde in "+strings[0]+" umgenannt");
-            player.setCustomName(strings[0]);
+            player.sendMessage(strings[0]+" heißt nun "+strings[1]);
+            Player toRenamePlayer = Bukkit.getPlayer(strings[0]);
+           // toRenamePlayer.setCustomName(strings[1]);
+            toRenamePlayer.setPlayerListName(strings[1]);
+            toRenamePlayer.setDisplayName(strings[1]);
+
+
         } else {
             sender.sendMessage("Dieses Kommand darf du nur als Spieler benutzen");
         }
@@ -20,4 +34,6 @@ public class RenameCommand implements CommandExecutor {
 
         return false;
     }
+
+
 }
